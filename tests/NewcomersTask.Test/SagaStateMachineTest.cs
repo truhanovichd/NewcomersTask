5a5484deb64a770a53b7a4525aa577f49aec7e92
Dmitry Truhanovich <d.truhanovich@godeltech.com>
2022-10-13 15:16:06 +0300

    Update tests.

--- tests/NewcomersTask.Test/SagaStateMachineTest.cs
+++ tests/NewcomersTask.Test/SagaStateMachineTest.cs
@@ -1,13 +1,10 @@
 ﻿using MassTransit;
 using MassTransit.Testing;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using NewcomersTask.DB;
 using NewcomersTask.Host;
 using NewcomersTask.Models;
 using NewcomersTask.Models.DB;
 using NUnit.Framework;
-using System.Reflection;
 
 namespace NewcomersTask.Test
 {
@@ -18,22 +15,11 @@ namespace NewcomersTask.Test
         public async Task When_CreateOrder_Expect_MoveOrderToStatusAwaitingPacking()
         {
             await using var provider = new ServiceCollection()
-                .AddMassTransitTestHarness(cfg =>
-                {
-                    cfg.AddSagaStateMachine<OrderStateMachine, OrderState>();
-                    //cfg.SetSagaRepositoryProvider(new ConnectionFactory().CreateContextForSQLite());
-
-                    cfg.AddDbContext<DbContext, SagaContext>((provider, builder) =>
-                    {
-                        builder.UseSqlite("DataSource=:memory:", m =>
-                        {
-                            m.MigrationsAssembly(Assembly.GetExecutingAssembly().GetName().Name);
-                            m.MigrationsHistoryTable($"__{nameof(SagaContext)}");
-                        });
-                    });
-                })
-                //.AddDbContext<SagaContext>(options => );
-                .BuildServiceProvider(true);
+            .AddMassTransitTestHarness(cfg =>
+            {
+                cfg.AddSagaStateMachine<OrderStateMachine, OrderState>();
+            })
+            .BuildServiceProvider(true);
 
             var harness = provider.GetRequiredService<ITestHarness>();
 
@@ -45,22 +31,101 @@ namespace NewcomersTask.Test
             await harness.Bus.Publish(new OrderCreated
             {
                 OrderId = sagaId,
-                OrderNumber = orderNumber,
-                OrderDate = DateTime.UtcNow,
-                CustomerName = "Test Customer Name",
-                CustomerSurname = "Test Customer Surname",
-                OrderSagaItems = new List<OrderItem> { new OrderItem { Price = 123.2m, Quantity = 1, Sku = "123Test"} }
+                OrderNumber = orderNumber
             });
 
-            //Assert.That(await harness.Consumed.Any<OrderItem>());
+            Assert.That(await harness.Consumed.Any<OrderCreated>());
 
             var sagaHarness = harness.GetSagaStateMachineHarness<OrderStateMachine, OrderState>();
 
-            //Assert.That(await sagaHarness.Consumed.Any<OrderItem>());
+            Assert.That(await sagaHarness.Consumed.Any<OrderCreated>());
 
             Assert.That(await sagaHarness.Created.Any(x => x.CorrelationId == sagaId));
 
             var instance = sagaHarness.Created.ContainsInState(sagaId, sagaHarness.StateMachine, sagaHarness.StateMachine.AwaitingPacking);
+            Assert.IsNotNull(instance, "Saga instance not found");
+            Assert.That(instance.OrderNumber, Is.EqualTo(orderNumber));
+        }
+
+        [Test]
+        public async Task When_UpdateOrder_Expect_MoveOrderToStatusPacked()
+        {
+            await using var provider = new ServiceCollection()
+            .AddMassTransitTestHarness(cfg =>
+            {
+                cfg.AddSagaStateMachine<OrderStateMachine, OrderState>();
+            })
+            .BuildServiceProvider(true);
+
+            var harness = provider.GetRequiredService<ITestHarness>();
+
+            await harness.Start();
+
+            var sagaId = Guid.NewGuid();
+            var orderNumber = 1234;
+
+            await harness.Bus.Publish(new OrderCreated
+            {
+                OrderId = sagaId,
+                OrderNumber = orderNumber
+            });
+
+            await harness.Bus.Publish(new OrderStatusChanged
+            {
+                CorrelationId = sagaId,
+                Status = Status.Packed
+            });
+
+            Assert.That(await harness.Consumed.Any<OrderCreated>());
+
+            var sagaHarness = harness.GetSagaStateMachineHarness<OrderStateMachine, OrderState>();
+
+            Assert.That(await sagaHarness.Consumed.Any<OrderStatusChanged>());
+
+            Assert.That(await sagaHarness.Created.Any(x => x.CorrelationId == sagaId));
+
+            var instance = sagaHarness.Created.ContainsInState(sagaId, sagaHarness.StateMachine, sagaHarness.StateMachine.Packed);
+            Assert.IsNotNull(instance, "Saga instance not found");
+            Assert.That(instance.OrderNumber, Is.EqualTo(orderNumber));
+        }
+
+        [Test]
+        public async Task When_CancelOrder_Expect_MoveOrderToStatusCancel()
+        {
+            await using var provider = new ServiceCollection()
+            .AddMassTransitTestHarness(cfg =>
+            {
+                cfg.AddSagaStateMachine<OrderStateMachine, OrderState>();
+            })
+            .BuildServiceProvider(true);
+
+            var harness = provider.GetRequiredService<ITestHarness>();
+
+            await harness.Start();
+
+            var sagaId = Guid.NewGuid();
+            var orderNumber = 1235;
+
+            await harness.Bus.Publish(new OrderCreated
+            {
+                OrderId = sagaId,
+                OrderNumber = orderNumber
+            });
+
+            await harness.Bus.Publish(new OrderCancelled
+            {
+                CorrelationId = sagaId
+            });
+
+            Assert.That(await harness.Consumed.Any<OrderCreated>());
+
+            var sagaHarness = harness.GetSagaStateMachineHarness<OrderStateMachine, OrderState>();
+
+            Assert.That(await sagaHarness.Consumed.Any<OrderCancelled>());
+
+            Assert.That(await sagaHarness.Created.Any(x => x.CorrelationId == sagaId));
+
+            var instance = sagaHarness.Created.ContainsInState(sagaId, sagaHarness.StateMachine, sagaHarness.StateMachine.Cancelled);
             Assert.IsNotNull(instance, "Saga instance not found");
             Assert.That(instance.OrderNumber, Is.EqualTo(orderNumber));
         }
